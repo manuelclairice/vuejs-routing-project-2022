a6b7141cbd6bf7a2ed3cd83d211a080d85760f2e
Manuel Clairice <m.clairice@rewe-group.at>
2022-08-22 16:52:56 +0200

    implement passing params as props

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to team 2</router-link>
   </section>
 </template>
 
@@ -17,6 +18,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem
   },
@@ -26,9 +28,8 @@ export default {
       members: [],
     };
   },
-  created() {
-    
-    const teamId = this.$route.params.teamId;
+  methods: {
+    loadTeamMembers(teamId) {
     const selectedTeam = this.teams.find(team => team.id === teamId);
     const members = selectedTeam.members;
     const selectedMembers = [];
@@ -38,6 +39,15 @@ export default {
     }
     this.members = selectedMembers;
     this.teamName = selectedTeam.name;
+    }
+  },
+  created() {
+    this.loadTeamMembers(this.teamId);
+  },
+  watch: {
+    teamId(newId) {
+      this.loadTeamMembers(newId);
+    }
   }
 };
 </script>
